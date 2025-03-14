6c7fbca6f3b7a7fb10c220c43a8dfa86a0cbb07c
Clarence Ho <ho.clarence@gmail.com>
2025-03-14 12:22:19 +0800

    Created Formula for UnderDoc CLI

--- underdoc.rb
+++ underdoc.rb
@@ -1,20 +1,19 @@
 class Underdoc < Formula
+    include Language::Python::Virtualenv
+
     desc "CLI for interacting with Underdoc API"
-    homepage "https://underdoc.ai" # Your project's homepage
+    homepage "https://underdoc.io" # Your project's homepage
     url "https://github.com/under-doc/underdoc-cli/archive/refs/tags/0.1.0.tar.gz" # Replace with your release URL
     sha256 "31eb239443e5e98e63203ceffd07144510f003268333a8c3ed2cb1c000435bb1" # Generate using shasum -a 256 <your-archive.tar.gz>
     license "MIT" # Replace with your license
   
-    depends_on "python@3.10" # Or your preferred Python version
+    depends_on "python@3.12" # Or your preferred Python version
   
     def install
-      libexec.install Dir["*"]
-      bin.install_symlink libexec/"underdoc/bin/underdoc" # Adjust the path to your executable
-      python3 = Formula["python@3.10"].opt_bin/"python3" # Or your preferred python version
-      system python3, "-m", "pip", "install", "-e", libexec
-    end
-  
-    test do
-      system "#{bin}/underdoc", "--help"
+        venv = virtualenv_create(libexec)
+        %w[click httpx pydantic].each do |r|
+            venv.pip_install resource(r)
+        end
+        venv.pip_install_and_link buildpath
     end
   end
