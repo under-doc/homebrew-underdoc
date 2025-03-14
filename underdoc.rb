67074f002e6783dc46b63b7d802da20f1cc56be0
Clarence Ho <ho.clarence@gmail.com>
2025-03-14 12:30:01 +0800

    Created Formula for UnderDoc CLI

--- underdoc.rb
+++ underdoc.rb
@@ -9,11 +9,22 @@ class Underdoc < Formula
   
     depends_on "python@3.12" # Or your preferred Python version
   
+    resource "click" do
+        url "https://files.pythonhosted.org/packages/b9/2e/0090cbf739cee7d23781ad4b89a9894a41538e4fcf4c31dcdd705b78eb8b/click-8.1.8.tar.gz"
+        sha256 "ed53c9d8990d83c2a27deae68e4ee337473f6330c040a31d4225c9574d16096a"
+    end
+    
+    resource "httpx" do
+        url "https://files.pythonhosted.org/packages/b1/df/48c586a5fe32a0f01324ee087459e112ebb7224f646c0b5023f5e79e9956/httpx-0.28.1.tar.gz"
+        sha256 "75e98c5f16b0f35b567856f597f06ff2270a374470a5c2392242528e3e3e42fc"
+    end
+
+    resource "pydantic" do
+        url "https://files.pythonhosted.org/packages/b7/ae/d5220c5c52b158b1de7ca89fc5edb72f304a70a4c540c84c8844bf4008de/pydantic-2.10.6.tar.gz"
+        sha256 "ca5daa827cce33de7a42be142548b0096bf05a7e7b365aebfa5f8eeec7128236"
+    end
+    
     def install
-        venv = virtualenv_create(libexec)
-        %w[click httpx pydantic].each do |r|
-            venv.pip_install resource(r)
-        end
-        venv.pip_install_and_link buildpath
+        virtualenv_install_with_resources
     end
   end
